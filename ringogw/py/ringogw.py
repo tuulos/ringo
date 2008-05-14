a7a65505d3ccee58ac91f7f2c3addd7034e5241e
Ville Tuulos <tuulos@cfront.nokiapaloalto.com>
2008-05-13 22:47:49 -0700

    made ringogw finally properly: A new Ringo-class encapsulates the connection state and makes it thread-safe

--- ringogw/py/ringogw.py
+++ ringogw/py/ringogw.py
@@ -3,9 +3,6 @@ import cjson, pycurl, cStringIO, time, re
 
 multi_head_re = re.compile("(\d+) (.*?) ")
 
-host = ""
-curl = None
-
 class ReplyException(Exception):
         pass
 
@@ -24,6 +21,7 @@ class DecodeJson:
                 self.buf.write(data)
         def output(self):
                 return cjson.decode(self.buf.getvalue())
+                self.host = host
 
 class DecodeMulti:
         def __init__(self, cb = None):
@@ -66,110 +64,101 @@ class DecodeMulti:
                 else:
                         return self.ret, self.out
 
-def sethost(url):
-        global host
-        if not url.startswith("http://"):
-                host = "http://" + url
-        host = url
+class Ringo:
+        def __init__(self, host, keep_alive = True):
+                if not host.startswith("http://"):
+                        host = "http://" + host
+                self.host = host
+                if keep_alive:
+                        self.curl = pycurl.Curl()
+                self.keep_alive = keep_alive
 
-def check_reply(reply):
-        if reply[0] != 200 or reply[1][0] != 'ok':
-                e = ReplyException("Invalid reply (code: %d): %s" %\
-                        (reply[0], reply[1]))
-                e.retcode = reply[0]
-                e.retvalue = reply[1]
-                raise e 
-        return reply[1][1:]
-
-def create(domain, nrepl, **kwargs):
-        kwargs['decoder'] = DecodeJson
-        url = "/mon/data/%s?create&nrepl=%d" % (domain, nrepl)
-        if 'noindex' in kwargs:
-                url += "&noindex=1"
-                del kwargs['noindex']
-        if 'keycache' in kwargs:
-                url += "&keycache=1"
-                del kwargs['keycache']
-        return check_reply(request(url, "", **kwargs))[0]
-
-def put(domain, key, value, **kwargs):
-        kwargs['decoder'] = DecodeJson
-        return check_reply(request("/mon/data/%s/%s" % (domain, key),
-                value, **kwargs))
+        def request(self, url, data = None, verbose = False,
+                        retries = 0, decoder = DecodeJson):
         
-def get(domain, key, **kwargs):
-        url = "/mon/data/%s/%s" % (domain, key)
-        kwargs['data'] = None
-        if 'single' in kwargs:
-                kwargs['decoder'] = DecodeRaw
-                del kwargs['single']
-                code, val = request(url + "?single", **kwargs)
-                if code != 200:
-                        e = ReplyException("Invalid reply (code: %d)" % code)
-                        e.retcode = code
-                        raise e
-                return val
-        else:
-                if 'entry_callback' in kwargs:
-                        cb = kwargs['entry_callback']
-                        kwargs['decoder'] = lambda: DecodeMulti(cb)
-                        del kwargs['entry_callback']
+                if self.keep_alive:
+                        curl = self.curl
                 else:
-                        kwargs['decoder'] = DecodeMulti
-                return check_reply(request(url, **kwargs))[0]
+                        curl = pycurl.Curl()
 
+                if url.startswith("http://"):
+                        purl = url
+                else:
+                        purl = self.host + url
 
-# Although Curl's documentation suggests re-using a curl object for
-# multiple requests, doing it seems to slow things down considerably.
-# The first request is fast but requests after that are not (except 
-# some requests occasionally with the lighttpd/scgi combination). Weird.
-# TODO: Check whether TCP_NODELAY could help.
-def request(url, data = None, verbose = False, keep_alive = True, 
-        retries = 0, decoder = DecodeJson):
-        global curl
-        if url.startswith("http://"):
-                purl = url
-        else:
-                purl = host + url
+                curl.setopt(curl.URL, purl)
+                if data == None:
+                        curl.setopt(curl.HTTPGET, 1)
+                else:
+                        curl.setopt(curl.POST, 1)
+                        curl.setopt(curl.POSTFIELDS, data)
+                        curl.setopt(curl.HTTPHEADER, ["Expect:"])
+                dec = decoder()
+                curl.setopt(curl.WRITEFUNCTION, dec.write)
+                try:
+                        curl.perform()
+                except pycurl.error, x:
+                        if verbose:
+                                print "Pycurl.error:", x
+                        return x
 
-        if not (curl and keep_alive):
-                curl = pycurl.Curl()
-        curl.setopt(curl.URL, purl)
-        if data == None:
-                curl.setopt(curl.HTTPGET, 1)
-        else:
-                curl.setopt(curl.POST, 1)
-                curl.setopt(curl.POSTFIELDS, data)
-                curl.setopt(curl.HTTPHEADER, ["Expect:"])
-        dec = decoder()
-        curl.setopt(curl.WRITEFUNCTION, dec.write)
-        try:
-                curl.perform()
-        except pycurl.error, x:
+                code = curl.getinfo(curl.HTTP_CODE)
                 if verbose:
-                        print "Pycurl.error:", x
-                return x
+                        print "Request took %.2fms" %\
+                                (curl.getinfo(curl.TOTAL_TIME) * 1000.0)
 
-        code = curl.getinfo(curl.HTTP_CODE)
-        if verbose:
-                print "Request took %.2fms" %\
-                        (curl.getinfo(curl.TOTAL_TIME) * 1000.0)
-        
-        if not keep_alive:
-                curl = None
+                # Request timeout
+                if code == 408 and retries > 0:
+                        time.sleep(0.1)
+                        return self.request(url, data, verbose,
+                                        retries - 1, decoder)
+                else: 
+                        return code, dec.output()
 
-        # Request timeout
-        if code == 408 and retries > 0:
-                time.sleep(0.1)
-                return request(url, data, verbose,
-                        keep_alive, retries - 1)
-        else: 
-                return code, dec.output()
 
-        
+        def check_reply(self, reply):
+                if reply[0] != 200 or reply[1][0] != 'ok':
+                        e = ReplyException("Invalid reply (code: %d): %s" %\
+                                (reply[0], reply[1]))
+                        e.retcode = reply[0]
+                        e.retvalue = reply[1]
+                        raise e 
+                return reply[1][1:]
 
+        def create(self, domain, nrepl, **kwargs):
+                kwargs['decoder'] = DecodeJson
+                url = "/mon/data/%s?create&nrepl=%d" % (domain, nrepl)
+                if 'noindex' in kwargs:
+                        url += "&noindex=1"
+                        del kwargs['noindex']
+                if 'keycache' in kwargs:
+                        url += "&keycache=1"
+                        del kwargs['keycache']
+                return self.check_reply(self.request(url, "", **kwargs))[0]
+
+        def put(self, domain, key, value, **kwargs):
+                kwargs['decoder'] = DecodeJson
+                return self.check_reply(self.request("/mon/data/%s/%s" %\
+                        (domain, key), value, **kwargs))
                 
-
-
-
-
+        def get(self, domain, key, **kwargs):
+                url = "/mon/data/%s/%s" % (domain, key)
+                kwargs['data'] = None
+                if 'single' in kwargs:
+                        kwargs['decoder'] = DecodeRaw
+                        del kwargs['single']
+                        code, val = self.request(url + "?single", **kwargs)
+                        if code != 200:
+                                e = ReplyException("Invalid reply (code: %d)"\
+                                        % code)
+                                e.retcode = code
+                                raise e
+                        return val
+                else:
+                        if 'entry_callback' in kwargs:
+                                cb = kwargs['entry_callback']
+                                kwargs['decoder'] = lambda: DecodeMulti(cb)
+                                del kwargs['entry_callback']
+                        else:
+                                kwargs['decoder'] = DecodeMulti
+                        return self.check_reply(self.request(url, **kwargs))[0]
