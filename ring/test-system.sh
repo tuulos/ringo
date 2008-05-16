3d6c84ffb6bbabe9f6b5d5777ad107a2aac1ec75
Ville Tuulos <tuulos@cfront.nokiapaloalto.com>
2008-05-15 18:49:36 -0700

    added a check to test-system.sh that makes sure that ringogw is running

--- ring/test-system.sh
+++ ring/test-system.sh
@@ -1,4 +1,10 @@
 export PATH=$PATH:. 
 export PYTHONPATH=../ringogw/py
 rm -Rf /tmp/ringotest-*
-python -u test/system_test.py localhost:15000 $@
+
+if ! wget -O - localhost:15000/mon/ring/reset >/dev/null 2>/dev/null
+then
+        echo "Ringogw doesn't seem to be running at http://localhost:15000"
+else
+        python -u test/system_test.py localhost:15000 $@
+fi
