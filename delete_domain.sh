faf70fbca832cf692e9534892d83d4ce344e6274
Ringo <ringo@nxfront.nokiapaloalto.com>
2008-05-22 23:12:10 -0700

    two new helper scripts

---
+++ delete_domain.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+	echo "Usage: delete_domain.sh [node] [ringo_root] [domainid]"
+	exit 1
+fi
+
+ssh $1 "find $2 -iname 'rdomain-$3' -exec rm -Rf \{\} \;"
---
+++ list_domains.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+	echo "Usage: list_domains.sh [node] [ringo_root]"
+	exit 1
+fi
+
+for f in `ssh $1 find $2 -iname 'rdomain-*'`; do
+	T=`basename $f`
+	echo ${T:8}
+done
