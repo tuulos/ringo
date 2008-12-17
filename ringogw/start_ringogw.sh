f156a8fb2154fe05c8397cc47ac5be5ad50e88fa
Ville Tuulos <tuulos@trurl.(none)>
2008-12-17 01:58:01 -0800

    Start lighttpd in start_ringogw.sh script

--- ringogw/start_ringogw.sh
+++ ringogw/start_ringogw.sh
@@ -1,6 +1,7 @@
 
 MODE=mochi
 PORT=15000
+SCGI=1
 
 if [ ! -z $SCGI ]
 then
@@ -10,6 +11,8 @@ then
 fi
 
 export HEART_COMMAND="$0 $@"
+
+PATH=$PATH:/usr/sbin lighttpd -f lighttpd.conf -D &
 
 PATH=.:$PATH erl -heart -noshell -detached +K true -smp on -sname ringogw -setcookie ringobingo -pa ../mochiweb/ebin -pa ../ring/ebin -pa ebin -pa src -boot ringogw -ringogw httpmode $MODE -ringogw port $PORT -ringogw docroot \"web\" -ringogw dynroot \"mon\" -kernel error_logger '{file, "ringogw.log"}' -eval "[handle_ring, handle_domains, handle_data]"
 
